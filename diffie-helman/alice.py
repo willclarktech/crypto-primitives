c70c319d5191035db642339eb4ee4c098216098f
willclarktech <williamclark.home@gmail.com>
2017-03-18 19:44:56 +0100

    add main functions for diffie scripts

--- diffie-helman/alice.py
+++ diffie-helman/alice.py
@@ -3,16 +3,22 @@ sys.path.append(os.path.abspath(os.path.join('primes')))
 from primes import get_large_prime
 from random import randint
 
-g = 2
-p = get_large_prime()
-print("g: ", g)
-print("p: ", p)
+def main(min_a=2000, max_a=3000):
+	g = 2
+	p = get_large_prime()
+	print("g: ", g)
+	print("p: ", p)
 
-a = randint(2000, 3000)
-A = g**a % p
-print("A: ", A)
+	a = randint(min_a, max_a)
+	A = g**a % p
+	print("A: ", A)
 
-B = int(input("Enter B now: "))
-s = B**a % p
-print("RESULT: ", s)
+	B = int(input("Enter B now: "))
+	s = B**a % p
+	print("RESULT: ", s)
+
+if __name__ == "__main__":
+	min_a = int(sys.argv[1]) if len(sys.argv) >= 3 else 2000
+	max_a = int(sys.argv[2]) if len(sys.argv) >= 3 else 3000
+	main(min_a, max_a)
 
--- diffie-helman/bob.py
+++ diffie-helman/bob.py
@@ -1,13 +1,20 @@
+import sys
 from random import randint
 
-g = int(input("Enter g now: "))
-p = int(input("Enter p now: "))
+def main(min_b=2000, max_b=3000):
+	g = int(input("Enter g now: "))
+	p = int(input("Enter p now: "))
 
-b = randint(2000, 3000)
-B = g**b % p
-print("B: ", B)
+	b = randint(2000, 3000)
+	B = g**b % p
+	print("B: ", B)
 
-A = int(input("Enter A now: "))
-s = A**b % p
-print("RESULT: ", s)
+	A = int(input("Enter A now: "))
+	s = A**b % p
+	print("RESULT: ", s)
+
+if __name__ == "__main__":
+	min_b = int(sys.argv[1]) if len(sys.argv) >= 3 else 2000
+	max_b = int(sys.argv[2]) if len(sys.argv) >= 3 else 2000
+	main(min_b, max_b)
 
