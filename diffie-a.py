8388f8ff624ddfd7c7ac42844bb127a9381f71bf
willclarktech <williamclark.home@gmail.com>
2017-03-18 16:37:29 +0100

    update and tidy diffie scripts

--- diffie-a.py
+++ diffie-a.py
@@ -1,15 +1,16 @@
 from primes import get_large_prime
 from random import randint
 
-g, p = get_large_prime()[-2:]
+g = 2
+p = get_large_prime()
 print("g: ", g)
 print("p: ", p)
 
 a = randint(2000, 3000)
 A = g**a % p
-print("A:", A)
+print("A: ", A)
 
 B = int(input("Enter B now: "))
 s = B**a % p
-print("RESULT:", s)
+print("RESULT: ", s)
 
--- diffie-b.py
+++ diffie-b.py
@@ -5,9 +5,9 @@ print(g, p)
 
 b = randint(2000, 3000)
 B = g**b % p
-print("B:", B)
+print("B: ", B)
 
 A = int(input("Enter A now: "))
 s = A**b % p
-print("RESULT", s)
+print("RESULT: ", s)
 
