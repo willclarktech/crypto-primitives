d3d14eae7be779deab11b52844bee5c2479a5b37
willclarktech <williamclark.home@gmail.com>
2017-03-18 16:27:36 +0100

    use a generator to get primes

--- primes.py
+++ primes.py
@@ -5,30 +5,26 @@ from random import randint
 def check_prime(n, primes):
 	for prime in primes:
 		if sqrt(n) < prime:
-			break
+			return True
 		elif n % prime == 0:
 			return False
-	return True
 
 def get_next_prime(primes):
 	n = primes[-1] + 2
-	found = False
-	while found == False:
+	while True:
 		if check_prime(n, primes):
-			found = True
-		else:
-			n+=2
-	return n
-
-def find_primes_in_range(i, j):
-	n = randint(i, j)
-	primes = [2, 3]
-	while primes[-1] < n:
-		primes.append(get_next_prime(primes))
-	return primes
+			primes.append(n)
+			yield n
+		n+=2
 
 def get_large_prime(i=100, j=300):
-	return find_primes_in_range(i, j)[-1]
+	n = randint(i, j)
+	primes = [2, 3]
+	next_prime = 3
+	prime_generator = get_next_prime(primes)
+	while next_prime < n:
+		next_prime = next(prime_generator)
+	return next_prime
 
 if __name__ == "__main__":
 	i = int(sys.argv[1]) if len(sys.argv) >= 3 else 100
