b08174b7391bceb61e3ba04de6065ed7c2f563f5
willclarktech <williamclark.home@gmail.com>
2017-03-18 19:47:51 +0100

    add option to set g to diffie alice

--- diffie-helman/alice.py
+++ diffie-helman/alice.py
@@ -3,8 +3,7 @@ sys.path.append(os.path.abspath(os.path.join('primes')))
 from primes import get_large_prime
 from random import randint
 
-def main(min_a=2000, max_a=3000):
-	g = 2
+def main(min_a=2000, max_a=3000, g=2):
 	p = get_large_prime()
 	print("g: ", g)
 	print("p: ", p)
@@ -20,5 +19,6 @@ def main(min_a=2000, max_a=3000):
 if __name__ == "__main__":
 	min_a = int(sys.argv[1]) if len(sys.argv) >= 3 else 2000
 	max_a = int(sys.argv[2]) if len(sys.argv) >= 3 else 3000
-	main(min_a, max_a)
+	g = int(sys.argv[3]) if len(sys.argv) >= 4 else 2
+	main(min_a, max_a, g)
 
