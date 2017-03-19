fad7a98607151d1ee93da2a6f26ab59c97ff399f
willclarktech <williamclark.home@gmail.com>
2017-03-19 17:04:28 +0100

    add basic redis commands

--- redis_tools/main.py
+++ redis_tools/main.py
@@ -2,7 +2,18 @@
 
 import redis
 
+def init_redis():
+	return redis.StrictRedis(host='localhost', port=6379, db=0)
+
 def get(key):
-	r = redis.StrictRedis(host='localhost', port=6379, db=0)
+	r = init_redis()
 	return r.get(key)
+
+def set(key, value):
+	r = init_redis()
+	return r.set(key, value)
+
+def delete(key):
+	r = init_redis()
+	return r.delete(key)
 
