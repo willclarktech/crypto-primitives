4a880184aff120a3b059e0ab8f251c9b463f6693
willclarktech <williamclark.home@gmail.com>
2017-03-19 18:51:10 +0100

    tidy up printing

--- diffie_helman/alice.py
+++ diffie_helman/alice.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
 import os, sys
-sys.path.append(os.path.abspath(os.path.join('primes')))
-sys.path.append(os.path.abspath(os.path.join('redis_tools')))
+sys.path.append(os.path.abspath(os.path.join("primes")))
+sys.path.append(os.path.abspath(os.path.join("redis_tools")))
 from primes import get_large_prime
 from redis_tools import setter, waiter, deleter
 from random import randint
 
 def main(min_a=2000, max_a=3000, g=2):
-	p = get_large_prime()
-	redis_setter = setter(['g', g])
+	redis_setter = setter(["g", g])
 	redis_setter.send(None)
-	redis_setter.send(['p', p])
-	print("g: ", g)
-	print("p: ", p)
+	print("Set g")
+
+	p = get_large_prime()
+	redis_setter.send(["p", p])
+	print("Set p")
 
 	a = randint(min_a, max_a)
 	A = g**a % p
-	redis_setter.send(['A', A])
-	print("A: ", A)
+	redis_setter.send(["A", A])
+	print("Set A")
 
 	print("Waiting for B...")
-	redis_waiter = waiter('B')
+	redis_waiter = waiter("B")
 	B = int(redis_waiter.send(None))
-	print("B: ", B)
+	print("Got B")
 	redis_deleter = deleter("B")
 	redis_deleter.send(None)
 
--- diffie_helman/bob.py
+++ diffie_helman/bob.py
@@ -8,13 +8,13 @@ def main(min_b=2000, max_b=3000):
 	print("Waiting for g...")
 	redis_waiter = waiter("g")
 	g = int(redis_waiter.send(None))
-	print("g: ", g)
+	print("Got g")
 	redis_deleter = deleter("g")
 	redis_deleter.send(None)
 
 	print("Waiting for p...")
 	p = int(redis_waiter.send("p"))
-	print("p: ", p)
+	print("Got p")
 	redis_deleter.send("p")
 
 	b = randint(2000, 3000)
@@ -22,10 +22,11 @@ def main(min_b=2000, max_b=3000):
 
 	redis_setter = setter(["B", B])
 	redis_setter.send(None)
-	print("B: ", B)
+	print("Set B")
 
 	print("Waiting for A...")
 	A = int(redis_waiter.send("A"))
+	print("Got A")
 	redis_deleter.send("A")
 
 	s = A**b % p
