dc563cc8dabc369e05c5dd03529f0c8a2a999acb
willclarktech <williamclark.home@gmail.com>
2017-03-19 17:20:13 +0100

    use generators to interact with redis

--- redis_tools/main.py
+++ redis_tools/main.py
@@ -1,19 +1,21 @@
 #!/bin/sh
-
 import redis
 
 def init_redis():
 	return redis.StrictRedis(host='localhost', port=6379, db=0)
 
-def get(key):
+def getter(key):
 	r = init_redis()
-	return r.get(key)
+	while True:
+		key = yield r.get(key)
 
-def set(key, value):
+def setter(key, value):
 	r = init_redis()
-	return r.set(key, value)
+	while True:
+		key, value = yield r.set(key, value)
 
-def delete(key):
+def deleter(key):
 	r = init_redis()
-	return r.delete(key)
+	while True:
+		key = yield r.delete(key)
 
