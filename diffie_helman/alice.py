4a0872b8108037adc5b5afb18c6b77e1a841624f
willclarktech <williamclark.home@gmail.com>
2017-03-19 18:03:07 +0100

    start using redis in diffie alice

--- diffie_helman/alice.py
+++ diffie_helman/alice.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 import os, sys
 sys.path.append(os.path.abspath(os.path.join('primes')))
+sys.path.append(os.path.abspath(os.path.join('redis_tools')))
 from primes import get_large_prime
+from redis_tools import setter, getter
 from random import randint
 
 def main(min_a=2000, max_a=3000, g=2):
 	p = get_large_prime()
+	redis_setter = setter(['g', g])
+	redis_setter.send(None)
+	redis_setter.send(['p', p])
 	print("g: ", g)
 	print("p: ", p)
 
 	a = randint(min_a, max_a)
 	A = g**a % p
+	redis_setter.send(['A', A])
 	print("A: ", A)
 
-	B = int(input("Enter B now: "))
+	redis_getter = getter('B')
+	B = int(redis_getter.send(None))
 	s = B**a % p
 	print("RESULT: ", s)
 
