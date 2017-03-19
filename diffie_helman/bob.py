87494b69b36bd683dc71fb065cb0b39e413348d9
willclarktech <williamclark.home@gmail.com>
2017-03-19 18:38:39 +0100

    wait for g, p, A in diffie bob

--- diffie_helman/bob.py
+++ diffie_helman/bob.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
-import sys
+import os, sys
+sys.path.append(os.path.abspath(os.path.join('redis_tools')))
+from redis_tools import setter, waiter
 from random import randint
 
 def main(min_b=2000, max_b=3000):
-	g = int(input("Enter g now: "))
-	p = int(input("Enter p now: "))
+	print("Waiting for g and p...")
+	redis_waiter = waiter("g")
+	g = int(redis_waiter.send(None))
+	p = int(redis_waiter.send("p"))
 
 	b = randint(2000, 3000)
 	B = g**b % p
+
+	redis_setter = setter(['B', B])
+	redis_setter.send(None)
 	print("B: ", B)
 
-	A = int(input("Enter A now: "))
+	print("Waiting for A...")
+	A = int(redis_waiter.send("A"))
 	s = A**b % p
 	print("RESULT: ", s)
 
