0574ff21e7a80e5573d52a0752aa957625cd941a
willclarktech <williamclark.home@gmail.com>
2017-03-30 21:47:43 +0200

    allow prime generator to print all primes

--- primes/primes.py
+++ primes/primes.py
@@ -18,18 +18,24 @@ def get_next_prime(primes):
 			yield n
 		n+=2
 
-def get_large_prime(i=1000, j=3000):
+def get_large_prime(print_all=False, i=1000, j=3000):
 	n = randint(i, j)
 	initial_primes = [2, 3]
+	if print_all:
+		print("2\n3")
 	next_prime = initial_primes[-1]
 	prime_generator = get_next_prime(initial_primes)
 	while next_prime < n:
 		next_prime = next(prime_generator)
+		if print_all:
+			print(next_prime)
 	return next_prime
 
 if __name__ == "__main__":
-	i = int(sys.argv[1]) if len(sys.argv) >= 3 else 1000
-	j = int(sys.argv[2]) if len(sys.argv) >= 3 else 3000
-	result = get_large_prime(i, j)
-	print(result)
+	print_all = sys.argv[1] == 'true' if len(sys.argv) >= 2 else False
+	i = int(sys.argv[2]) if len(sys.argv) >= 4 else 1000
+	j = int(sys.argv[3]) if len(sys.argv) >= 4 else 3000
+	result = get_large_prime(print_all, i, j)
+	if not print_all:
+		print(result)
 
