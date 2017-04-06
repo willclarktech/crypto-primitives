1224eae686b7515dbbd79e5a7ce34c98603b9e77
willclarktech <williamclark.home@gmail.com>
2017-04-07 00:59:46 +0200

    make set 1 challenge 5 useful from cli

--- cryptopals/set1/5_implement_repeating_key_xor.js
+++ cryptopals/set1/5_implement_repeating_key_xor.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 const fixedXor = require('./2_fixed_xor.js')
 
-const repeatingKeyXor = (key, input) => {
+const repeatingKeyXor = (key, input, encoding='utf8') => {
 	const repeatedKey = new Array(input.length)
 		.fill(null)
 		.map((_, i) => key[i % key.length])
 		.join('')
-	const buffers = [input, repeatedKey].map(s => new Buffer.from(s, 'utf8'))
+	const buffers = [input, repeatedKey].map(s => new Buffer.from(s, encoding))
 	return fixedXor(buffers)
+}
+
+if (require.main === module) {
+	if (process.argv.length < 4) throw new Error('Must provide a key and text')
+	const key = process.argv[2]
+	const text = process.argv[3]
+	const encoding = process.argv[4]
+	console.info(repeatingKeyXor(key, text, encoding))
 }
 
 module.exports = repeatingKeyXor
