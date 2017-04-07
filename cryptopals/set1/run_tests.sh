3d1a89d821f03a2dd222519827f4f8a8ee45f235
willclarktech <williamclark.home@gmail.com>
2017-04-07 12:46:39 +0200

    improve run_tests script

--- cryptopals/set1/run_tests.sh
+++ cryptopals/set1/run_tests.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-for TESTFILE in ./*.test.js
+DIR=${1:-'.'}
+echo "running tests in $PWD/$DIR"
+echo '...'
+
+for TESTFILE in $PWD/$DIR/*.test.js
 do
 	node $TESTFILE &
 done
 wait
+echo 'done'
 
