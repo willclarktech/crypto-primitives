7d165e0305d7efc7ef917b51c3c376e6a594aa15
willclarktech <williamclark.home@gmail.com>
2017-04-07 20:17:30 +0200

    remove maybe_convert_string_to_buffer helper

--- cryptopals/set1/helpers/maybe_convert_string_to_buffer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const maybeConvertStringToBuffer = (stringOrBuffer, encoding) =>
-	Buffer.isBuffer(stringOrBuffer)
-		? stringOrBuffer
-		: Buffer.from(stringOrBuffer, encoding)
-
-module.exports = maybeConvertStringToBuffer
-
