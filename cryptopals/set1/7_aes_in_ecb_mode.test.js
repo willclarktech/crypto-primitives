9b032858d85d36ae502f68dd2906526e511caaa9
willclarktech <will@willclark.tech>
2017-04-21 12:57:22 +0200

    adjust cryptopals set 1 challenge 8 test

--- cryptopals/set1/7_aes_in_ecb_mode.test.js
+++ cryptopals/set1/7_aes_in_ecb_mode.test.js
@@ -7,9 +7,10 @@ const testDecryptAes = () => {
   const fileString = fs.readFileSync('./challenge-data/7.txt', 'utf8')
   const input = Buffer.from(fileString, 'base64')
   const key = Buffer.from('YELLOW SUBMARINE')
+  const expected = fs.readFileSync('./challenge-data/7.decrypted.txt')
 
   const output = decryptAes(input)(key)
-  console.info(output.toString())
+  assert.ok(output.equals(expected))
 }
 
 testDecryptAes()
