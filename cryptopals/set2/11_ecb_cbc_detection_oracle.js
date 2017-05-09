39ba02399aa80627724c5d7cf4fec978fb472a7c
willclarktech <will@willclark.tech>
2017-05-09 17:45:40 +0200

    add set 2 challenge 11 encryption oracle function

--- cryptopals/set2/11_ecb_cbc_detection_oracle.js
+++ cryptopals/set2/11_ecb_cbc_detection_oracle.js
@@ -9,12 +9,22 @@ const addRandomBytes = message => {
 	return Buffer.concat([before, message, after])
 }
 
-const encryption_oracle = plaintext => {
+const encryption_oracle = plaintext => reveal => {
 	const key = createRandomAesKey()
-
+	const padded = addRandomBytes(plaintext)
+	const mode = Math.floor(Math.random() * 2)
+		? 'aes-128-cbc'
+		: 'aes-128-ecb'
+	reveal && console.info(mode)
+	const iv = mode === 'aes-128-cbc'
+		? createRandomAesKey()
+		: Buffer.alloc(0)
+	const cipher = crypto.createCipheriv(mode, key, iv)
+	return Buffer.concat([cipher.update(padded), cipher.final()])
 }
 
 module.exports = {
 	createRandomAesKey,
 	addRandomBytes,
+	encryption_oracle,
 }
--- cryptopals/set2/11_ecb_cbc_detection_oracle.test.js
+++ cryptopals/set2/11_ecb_cbc_detection_oracle.test.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
+const fs = require('fs')
 const assert = require('assert')
 const {
 	createRandomAesKey,
 	addRandomBytes,
+	encryption_oracle,
 } = require('./11_ecb_cbc_detection_oracle')
 
 assert.strictEqual(createRandomAesKey().length, 16)
 
 const addRandomBytesResult = addRandomBytes(Buffer.from('YELLOW SUBMARINE'))
 assert.ok(addRandomBytesResult.length >= 26 && addRandomBytesResult.length <= 36)
+
+const message = fs.readFileSync('./test-data/10.txt')
+const encryptionOracleResult = encryption_oracle(message)(true)
+assert.strictEqual(encryptionOracleResult.equals(message), false)
+
+
 
 console.info('11: shiny')
