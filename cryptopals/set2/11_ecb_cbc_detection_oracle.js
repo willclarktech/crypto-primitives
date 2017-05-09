b5e119e74a262de54518ab484d694562049ec1f9
willclarktech <will@willclark.tech>
2017-05-09 17:29:20 +0200

    add set 2 challenge 11 function to add random bytes

--- cryptopals/set2/11_ecb_cbc_detection_oracle.js
+++ cryptopals/set2/11_ecb_cbc_detection_oracle.js
@@ -2,7 +2,19 @@
 const crypto = require('crypto')
 
 const createRandomAesKey = () => crypto.randomBytes(16)
+const addRandomBytes = message => {
+	const fiveToTen = () => Math.floor(Math.random() * 6) + 5
+	const before = crypto.randomBytes(fiveToTen())
+	const after = crypto.randomBytes(fiveToTen())
+	return Buffer.concat([before, message, after])
+}
+
+const encryption_oracle = plaintext => {
+	const key = createRandomAesKey()
+
+}
 
 module.exports = {
 	createRandomAesKey,
+	addRandomBytes,
 }
--- cryptopals/set2/11_ecb_cbc_detection_oracle.test.js
+++ cryptopals/set2/11_ecb_cbc_detection_oracle.test.js
@@ -2,8 +2,12 @@
 const assert = require('assert')
 const {
 	createRandomAesKey,
+	addRandomBytes,
 } = require('./11_ecb_cbc_detection_oracle')
 
 assert.strictEqual(createRandomAesKey().length, 16)
+
+const addRandomBytesResult = addRandomBytes(Buffer.from('YELLOW SUBMARINE'))
+assert.ok(addRandomBytesResult.length >= 26 && addRandomBytesResult.length <= 36)
 
 console.info('11: shiny')
