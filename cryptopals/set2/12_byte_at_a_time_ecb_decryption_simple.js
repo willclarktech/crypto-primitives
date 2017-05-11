e0e6a058bcf517a19e805ac7666c93d2e503fa26
willclarktech <will@willclark.tech>
2017-05-11 20:15:00 +0100

    add set 2 challenge 12 solution

--- cryptopals/set2/12_byte_at_a_time_ecb_decryption_simple.js
+++ cryptopals/set2/12_byte_at_a_time_ecb_decryption_simple.js
@@ -37,12 +37,43 @@ const get_first_byte = oracle_function => block_size => {
 	const pad = Buffer.alloc(block_size - 1, 'A')
 	const target_block = oracle_function(pad).slice(0, block_size)
 	let i = 0
-	while (true) {
+	while (i < 256) {
 		const known = Buffer.concat([pad, Buffer.alloc(1, i)])
 		const candidate_block = oracle_function(known).slice(0, block_size)
 		if (candidate_block.equals(target_block)) return i
 		++i
 	}
+	return null
+}
+
+const get_nth_byte = oracle_function => block_size => preceding_bytes => {
+	const pad = Buffer.alloc(block_size - (preceding_bytes.length % block_size) - 1, 'A')
+	const block_index = Math.floor(preceding_bytes.length / block_size)
+	const target_block = oracle_function(pad)
+		.slice(block_index * block_size, (block_index + 1) * block_size)
+	let i = 0
+	while (i < 256) {
+		const done_blocks = Buffer.from(preceding_bytes.slice(0, block_index))
+		const focus_bytes = Buffer.from(preceding_bytes.slice(block_index))
+		const known = Buffer.concat([done_blocks, pad, focus_bytes, Buffer.alloc(1, i)])
+		const candidate_block = oracle_function(known)
+			.slice(block_index * block_size, (block_index + 1) * block_size)
+		if (candidate_block.equals(target_block)) return i
+		++i
+	}
+	return null
+}
+
+const decrypt_ecb = oracle_function => block_size => {
+	const l = oracle_function(Buffer.alloc(0)).length
+	return Buffer.from(new Array(l)
+		.fill()
+		.map((_, i) => i)
+		.reduce((accumulator, i) =>
+			[...accumulator, get_nth_byte(oracle_function)(block_size)(accumulator)]
+		, [])
+		.filter(b => b !== null)
+	)
 }
 
 module.exports = {
@@ -50,4 +81,5 @@ module.exports = {
 	get_block_size,
 	detect_ecb,
 	get_first_byte,
+	decrypt_ecb
 }
--- cryptopals/set2/12_byte_at_a_time_ecb_decryption_simple.test.js
+++ cryptopals/set2/12_byte_at_a_time_ecb_decryption_simple.test.js
@@ -6,6 +6,7 @@ const {
 	get_block_size,
 	detect_ecb,
 	get_first_byte,
+	decrypt_ecb,
 } = require('./12_byte_at_a_time_ecb_decryption_simple')
 
 const unknown = Buffer.from('Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK', 'base64')
@@ -18,6 +19,9 @@ const is_ecb = detect_ecb(oracle_function)(block_size)
 assert.ok(is_ecb)
 
 const first_byte = get_first_byte(oracle_function)(block_size)
-console.log('first byte of cipher text:', first_byte)
+assert.strictEqual(first_byte, unknown[0])
+
+const plaintext = decrypt_ecb(oracle_function)(block_size)
+assert.ok(plaintext.slice(0, unknown.length).equals(unknown))
 
 console.info('12: shiny')
