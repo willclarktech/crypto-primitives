0049d7d54559ca051e8762ec7f334e962636eab5
willclarktech <will@willclark.tech>
2017-05-12 15:37:38 +0100

    add set 2 challenge 13 functions to encrypt and decrypt

--- cryptopals/set2/13_ecb_cut_and_paste.js
+++ cryptopals/set2/13_ecb_cut_and_paste.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 const assert = require('assert')
+const crypto = require('crypto')
+const fs = require('fs')
+
+const key = Buffer.from(fs.readFileSync('./test-data/key_hex.txt', 'utf8'), 'hex')
+const iv = Buffer.alloc(0)
 
 const parse_key_value = str =>
 	str
@@ -26,8 +31,22 @@ const profile_for = email => {
   return encode_key_value(user)
 }
 
+const profile_for_ecb = email => {
+  const profile = Buffer.from(profile_for(email))
+  const cipher = crypto.createCipheriv('aes-128-ecb', key, iv)
+  return Buffer.concat([cipher.update(profile), cipher.final()])
+}
+
+const decrypt_user = ciphertext => {
+  const decipher = crypto.createDecipheriv('aes-128-ecb', key, iv)
+  const decrypted = Buffer.concat([decipher.update(ciphertext), decipher.final()])
+  return parse_key_value(decrypted.toString())
+}
+
 module.exports = {
 	parse_key_value,
 	create_user,
   profile_for,
+  profile_for_ecb,
+  decrypt_user,
 }
--- cryptopals/set2/13_ecb_cut_and_paste.test.js
+++ cryptopals/set2/13_ecb_cut_and_paste.test.js
@@ -4,7 +4,11 @@ const {
 	parse_key_value,
 	create_user,
   profile_for,
+  profile_for_ecb,
+  decrypt_user,
 } = require('./13_ecb_cut_and_paste')
+
+const email_input = 'foo@bar.com'
 
 const test_parse_key_value = () => {
 	const input = 'foo=bar&baz=qux&zap=zazzle'
@@ -18,26 +22,40 @@ const test_parse_key_value = () => {
 }
 
 const test_create_user = () => {
-	const normal_input = 'foo@bar.com'
-	const normal_output = create_user(normal_input)
-	assert.strictEqual(normal_output.email, normal_input)
+	const normal_output = create_user(email_input)
+	assert.strictEqual(normal_output.email, email_input)
 	assert.ok(typeof normal_output.uid === 'number')
 	assert.strictEqual(normal_output.role, 'user')
 
-	const sneaky_input = 'foo@bar.com&role=admin'
+	const sneaky_input = `${email_input}&role=admin`
 	const sneaky_output = create_user(sneaky_input)
 	assert.strictEqual(sneaky_output.email, 'foo@bar.comroleadmin')
 	assert.strictEqual(sneaky_output.role, 'user')
 }
 
 const test_profile_for = () => {
-  const input = 'foo@bar.com'
-  const output = profile_for(input)
+  const output = profile_for(email_input)
   assert.ok(output.match(/^email=foo@bar.com&uid=[\d]+&role=user$/))
+}
+
+const test_profile_for_ecb = () => {
+  const plain = Buffer.from(profile_for(email_input))
+  const output = profile_for_ecb(email_input)
+  assert.ok(!output.equals(plain))
+}
+
+const test_decrypt_user = () => {
+  const encrypted = profile_for_ecb(email_input)
+  const output = decrypt_user(encrypted)
+  console.log(output)
+  assert.strictEqual(output.email, email_input)
+  assert.strictEqual(output.role, 'user')
 }
 
 test_parse_key_value()
 test_create_user()
 test_profile_for()
+test_profile_for_ecb()
+test_decrypt_user()
 
 console.info('13: shiny')
