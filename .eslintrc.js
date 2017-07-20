31f2be34464c5fac4b237d23b77ecb39c1a0ea71
willclarktech <willclarktech@users.noreply.github.com>
2017-07-20 10:44:53 +0200

    Init npm and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	extends: 'airbnb-base',
+	rules: {
+		camelcase: 0,
+		'no-console': [2, { allow: ['error', 'info', 'warn'] }],
+		indent: [2, 'tab'],
+		'no-bitwise': 0,
+		'no-tabs': 0,
+		semi: [2, 'never'],
+	},
+}
