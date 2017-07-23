beacc6271781cacf10b402862701eb68d3046193
willclarktech <willclarktech@users.noreply.github.com>
2017-07-23 16:41:20 +0100

    Improve pkcs padding function

--- cryptopals/set2/9_pkcs_7_pad.js
+++ cryptopals/set2/9_pkcs_7_pad.js
@@ -1,9 +1,8 @@
 const assert = require('assert')
 
 const pkcs_7_pad = block_length => message => {
-	const pad_length = block_length - message.length
-	assert.strictEqual(pad_length >= 0, true, 'Cannot pad a message longer than the block size.')
-	const pad = Buffer.from(new Array(pad_length).fill(pad_length))
+	const pad_length = block_length - (message.length % block_length)
+	const pad = Buffer.alloc(pad_length, pad_length)
 	return Buffer.concat([message, pad])
 }
 
--- cryptopals/set2/9_pkcs_7_pad.test.js
+++ cryptopals/set2/9_pkcs_7_pad.test.js
@@ -2,10 +2,16 @@
 const assert = require('assert')
 const pkcs_7_pad = require('./9_pkcs_7_pad')
 
-const message = Buffer.from('YELLOW SUBMARINE')
 const block_length = 20
+
+const message = Buffer.from('YELLOW SUBMARINE')
 const output = pkcs_7_pad(block_length)(message)
 const expected = Buffer.concat([message, Buffer.from(new Array(4).fill(4))])
-
 assert.ok(output.equals(expected))
+
+const longer_message = Buffer.from(new Array(32).fill('YELLOW SUBMARINE').join(' '))
+const longer_output = pkcs_7_pad(block_length)(longer_message)
+const longer_expected = Buffer.concat([longer_message, Buffer.alloc(17, 17)])
+assert.ok(longer_output.equals(longer_expected))
+
 console.info('9: shiny')
