d645acc9a6cecb2f81ed4944d3c40d8f0646a9d0
willclarktech <willclarktech@users.noreply.github.com>
2017-10-09 23:16:28 +0200

    Add coursera week 1

---
+++ coursera/RESULTS.js
@@ -0,0 +1,11 @@
+[ 'The secret message is: When using a stream cipher, never use the key more than once',
+  'We can factor the number 15 with quantum computers. We can also factor the number 15 with a dog trained to bark three times - Robert Harley',
+  'Euler would probably enjoy that now his theorem becomes a corner stone of crypto - Annonymous on Euler\'s theorem',
+  'The nice thing about Keeyloq is now we cryptographers can drive a lot of fancy cars - Dan Boneh',
+  'The ciphertext produced by a weak encryption algorithm looks as good as ciphertext produced by a strong encryption algorithm - Philip Zimmermann',
+  'You don\'t want to buy a set of car keys from a guy who specializes in stealing cars - Marc Rotenberg commenting on Clipper',
+  'There are two types of cryptography - that which will keep secrets safe from your little sister, and that which will keep secrets safe from your government - Bruce Schneier',
+  'There are two types of cyptography: one that allows the Government to use brute force to break the code, and one that requires the Government to use brute force to break yo',
+  'We can see the point where the chip is unhappy if a wrong bit is sent and consumes more power from the environment - Adi Shamir',
+  'A (private-key)  encryption scheme states 3 algorithms, namely a procedure for generating keys, a procedure for encrypting, and a procedure for decrypting.�',
+  ' The Concise OxfordDictionary (2006) deﬁnes crypto as the art of  writing o r solving codes. ' ][0];
